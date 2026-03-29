ca5d2066be4ac40421ee61345ca6b956c16554e2
geekyfirebird <geekyfirebird@gmail.com>
2026-03-29 14:27:49 -0400

    Update prompt and response tokens printout

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ if api_key is None:
 
 client = genai.Client(api_key=api_key)
 
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
 response = client.models.generate_content(
     model = 'gemini-2.5-flash',
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents = prompt
 )
 
-print(response.text)
+response_metadata = response.usage_metadata
+
+print(f"User Prompt: {prompt}")
+
+if response_metadata is None:
+    raise RuntimeError("No metadata available")
+else:
+    print(f"""
+          Prompt Tokens: {response_metadata.prompt_token_count}\n 
+          Response Tokens: {response_metadata.candidates_token_count}
+    """)
+
+print(f"Response: {response.text}")
