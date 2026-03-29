460765ead6f2b0c6d9ec5a0e1fda4c11cc6c78a6
geekyfirebird <geekyfirebird@gmail.com>
2026-03-29 14:57:46 -0400

    Update and organize layout

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import os
+
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key is None:
-    raise RuntimeError("No api_key found")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("No api_key found")
 
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
-response = client.models.generate_content(
-    model = 'gemini-2.5-flash',
-    contents = prompt
-)
+    response = client.models.generate_content(
+        model = 'gemini-2.5-flash',
+        contents = prompt
+    )
 
-response_metadata = response.usage_metadata
+    response_metadata = response.usage_metadata
 
-print(f"User Prompt: {prompt}")
+    if response_metadata is None:
+        raise RuntimeError("No metadata available")
+  
+    print("User prompt:", prompt)
+    print("Prompt tokens:", response_metadata.prompt_token_count) 
+    print("Response tokens:", response_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
-if response_metadata is None:
-    raise RuntimeError("No metadata available")
-else:
-    print(f"""
-          Prompt Tokens: {response_metadata.prompt_token_count}\n 
-          Response Tokens: {response_metadata.candidates_token_count}
-    """)
-
-print(f"Response: {response.text}")
+if __name__ == "__main__":
+    main()
