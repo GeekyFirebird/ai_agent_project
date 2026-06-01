0fdbaf1c439e6da5e7221174a8a3d6f217cd4a37
geekyfirebird <geekyfirebird@gmail.com>
2026-06-01 15:34:16 -0400

    Setup client and token count

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("Key not found")
+
+    client = genai.Client(api_key=api_key)
+
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=prompt
+    )
+
+    if not response.usage_metadata:
+        raise RuntimeError("No metadata available")
+
+    prompt_tokens = response.usage_metadata.prompt_token_count #shows the number of tokens in the prompt that was sent to the model
+    response_tokens = response.usage_metadata.candidates_token_count #shows the number of tokens in the model's response
+
+
+
+    print(f"""
+        User prompt: {prompt}
+        Prompt tokens: {prompt_tokens}
+        Response tokens: {response_tokens}
+        Response: {response.text}
+    """)
+
+
+if __name__ == "__main__":
+    main()
+
