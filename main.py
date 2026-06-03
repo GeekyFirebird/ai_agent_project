30c47b5bb593b018ec3a38b8d4ae6776b425aa33
geekyfirebird <geekyfirebird@gmail.com>
2026-06-03 15:31:40 -0400

    Add message list and create seperate gen function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
@@ -17,16 +18,18 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    # messages:  list[types.Content] = [
-    #     types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
-    # ]
+    messages:  list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    generate_content(client, messages)
     
 
-    # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+def generate_content(client: genai.Client, messages: list[types.Content]) -> None:
 
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
-        contents=args.user_prompt
+        contents=messages
     )
 
     if not response.usage_metadata:
@@ -34,8 +37,6 @@ def main():
 
     prompt_tokens = response.usage_metadata.prompt_token_count #shows the number of tokens in the prompt that was sent to the model
     response_tokens = response.usage_metadata.candidates_token_count #shows the number of tokens in the model's response
-
-
 
     print(f"""
         User prompt: {args.user_prompt}
