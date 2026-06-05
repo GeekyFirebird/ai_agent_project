ca56588519777bef6e879a74504e1ad3e5df8ce2
geekyfirebird <geekyfirebird@gmail.com>
2026-06-05 15:49:28 -0400

    Update get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,21 +6,20 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     # working_directory will be set by us
     try:
         #1. Check to see if directory is in working_directory
-        working_dir_abs = os.path.abspath(working_directory)
+        abs_working_dir = os.path.abspath(working_directory)
         # print(working_dir_abs)
-        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
         # print(target_dir)
-
-        if os.path.isdir(directory):
-            print(f'Success: "{directory}" is within the working directory')
-        else:
-            print(f'Error: "{directory}" is not a directory')
 
         #2. Check if target_dir falls within absolute working_directory
         # Will be True or False
-        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-        if not valid_target_dir:
-            f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if os.path.commonpath([abs_working_dir, target_dir]) != abs_working_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        return f'Success: "{directory}" is within the working directory'
+
     except Exception as e:
         return f"Error: {e}"
 
